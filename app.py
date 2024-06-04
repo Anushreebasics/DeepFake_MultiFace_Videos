fcee8d7902399ec4b4d9456390ba0fcb257a0d29
ANUSHREE BONDIA <122273783+Anushreebasics@users.noreply.github.com>
2024-06-04 15:24:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def model_predict(img_path, model):
     preds = model.predict(x)
     return preds
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+# if __name__ == '__main__':
+    # app.run(host='127.0.0.1', port=5000)
